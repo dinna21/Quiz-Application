b83682f0003bc950f3f0ffacd9f216a56f6688a2
dinna21 <ransikadinura@gmail.com>
2025-04-15 22:58:04 +0530

    I create new questions.js file and add all of the question from that code all the qeustions are in that file I did this for code readability

---
+++ questions.js
@@ -0,0 +1,38 @@
+export const questions = [
+    {
+        question: "which is the largest animal in the World?",
+        answers: [
+            {text: "Shark",correct: false},
+            {text: "Blue Whale",correct: true},
+            {text: "Elephant",correct: false},
+            {text: "Giraffe",correct: false}
+        ]
+    },
+    {
+        question: "which is the Smallest animal in the World?",
+        answers: [
+            {text: "Rat",correct: true},
+            {text: "Blue Whale",correct: false},
+            {text: "Elephant",correct: false},
+            {text: "Giraffe",correct: false}
+        ]
+    },
+    {
+        question: "which is the most famous vehicle in the World?",
+        answers: [
+            {text: "toyota",correct: false},
+            {text: "Tesla",correct: true},
+            {text: "Nissan",correct: false},
+            {text: "Honda",correct: false}
+        ]
+    },
+    {
+        question: "which is the most famous Bike in the World?",
+        answers: [
+            {text: "Dukati",correct: false},
+            {text: "BMW",correct: true},
+            {text: "Nissan",correct: false},
+            {text: "Honda",correct: false}
+        ]
+    }
+]
--- script.js
+++ script.js
@@ -1,41 +1,4 @@
-const questions = [
-    {
-        question: "which is the largest animal in the World?",
-        answers: [
-            {text: "Shark",correct: false},
-            {text: "Blue Whale",correct: true},
-            {text: "Elephant",correct: false},
-            {text: "Giraffe",correct: false}
-        ]
-    },
-    {
-        question: "which is the Smallest animal in the World?",
-        answers: [
-            {text: "Rat",correct: true},
-            {text: "Blue Whale",correct: false},
-            {text: "Elephant",correct: false},
-            {text: "Giraffe",correct: false}
-        ]
-    },
-    {
-        question: "which is the most famous vehicle in the World?",
-        answers: [
-            {text: "toyota",correct: false},
-            {text: "Tesla",correct: true},
-            {text: "Nissan",correct: false},
-            {text: "Honda",correct: false}
-        ]
-    },
-    {
-        question: "which is the most famous Bike in the World?",
-        answers: [
-            {text: "Dukati",correct: false},
-            {text: "BMW",correct: true},
-            {text: "Nissan",correct: false},
-            {text: "Honda",correct: false}
-        ]
-    }
-]
+import { questions } from './questions.js';
 const questionElement = document.getElementById("quiz");
 const answerButton = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next");
